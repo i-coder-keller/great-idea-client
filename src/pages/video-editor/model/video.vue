314a00ab100daecc03b2e9003f5dafcbf14d4636
yongzhi.liu <yongzhi1.liu@gmail.com>
2022-08-17 23:15:23 +0800

    剪裁

--- src/pages/video-editor/model/video.vue
+++ src/pages/video-editor/model/video.vue
@@ -1,17 +1,12 @@
 <template>
   <div class="video-ref">
     <div class="video-proxy">
-      <video
-        class="video-player"
-        ref="videoRef"
-        :src="props.videoUrl"
-        @canplaythrough="canplaythrough"
-        @timeupdate="timeUpdate"
-        :key="data.n"
-      ></video>
-      <div class="canvas-proxy">
-        <canvas id="canvasRef" ref="canvasRef"></canvas>
-      </div>
+      <video class="videoRef" ref="videoRef" @canplaythrough="canplaythrough" @timeupdate="timeUpdate">
+        <source :src="videoUrl">
+      </video>
+    </div>
+    <div class="canvas-proxy">
+      <canvas ref="canvasRef" id="canvasRef"></canvas>
     </div>
   </div>
 </template>
@@ -28,7 +23,7 @@ interface Props {
 interface Reactive {
  n: number;
  canvas: fabric.Canvas | null;
- rect: fabric.rect | null;
+ rect: fabric.Rect | null;
  cWidth: number;
  cHeight: number;
 }
@@ -48,58 +43,35 @@ const init = () => {
  */
 const initFabric = () => {
   data.canvas = new fabric.Canvas('canvasRef')
-  data.canvas.backgroundColor = 'rgba(34, 47, 62,.3)'
   data.rect = new fabric.Rect({
     type: 'rect',
     cornerSize: 10,
-    borderColor: '#0abde3',
+    borderColor: '#0984e3',
     cornerStyle: 'circle',
-    selectable: true,
+    cornerColor: '#55efc4',
     hasControls: true,
     backgroundColor: 'rgba(255, 255, 255, 0)',
-    selectionBackgroundColor: 'rgba(255, 255, 255, 0)',
-    left: 0,
-    top: 0,
-    width: videoRef.value.clientWidth,
-    height: videoRef.value.clientHeight,
-    fill: 'rgba(255, 255, 255, 0)'
-  })
-  nextTick(() => {
-    data.rect.setControlsVisibility({
-      mtr: false
-    })
+    left: 5,
+    top: 5,
+    width: videoRef.value.clientWidth - 1,
+    height: videoRef.value.clientHeight - 1,
+    fill: 'rgba(255, 255, 255, 0)',
+    minScaleLimit: 0.1
+  }) as fabric.Rect
+  data.rect.setControlsVisibility({
+    mtr: false
   })
   data.canvas.add(data.rect)
   data.canvas.setActiveObject(data.rect)
-  data.canvas.on('object:moving', (evt: fabric.IEvent) => disableMover(evt))
-  data.canvas.on('object:scaling', (evt: fabric.IEvent) => disableScale(evt))
-}
-const disableMover = (evt: fabric.IEvent) => {
-  const obj = evt.target as any
-  if (obj.currentHeight > obj.canvas.height || obj.currentWidth > obj.canvas.width) {
-    return
-  }
-  obj.setCoords()
-  if (obj.getBoundingRect().top < 0 || obj.getBoundingRect().left < 0) {
-    obj.top = Math.max(obj.top, obj.top - obj.getBoundingRect().top)
-    obj.left = Math.max(obj.left, obj.left - obj.getBoundingRect().left)
-  }
-  if (obj.getBoundingRect().top + obj.getBoundingRect().height > obj.canvas.height || obj.getBoundingRect().left + obj.getBoundingRect().width > obj.canvas.width) {
-    obj.top = Math.min(obj.top, obj.canvas.height - obj.getBoundingRect().height + obj.top - obj.getBoundingRect().top)
-    obj.left = Math.min(obj.left, obj.canvas.width - obj.getBoundingRect().width + obj.left - obj.getBoundingRect().left)
-  }
 }
 
-const disableScale = (evt: any) => {
-  // TODO
-}
 /**
  * 视频加载完毕
  */
 const canplaythrough = () => {
   props.setDuration(videoRef.value.duration)
-  canvasRef.value.width = videoRef.value.clientWidth + 1
-  canvasRef.value.height = videoRef.value.clientHeight
+  canvasRef.value.width = videoRef.value.clientWidth + 10
+  canvasRef.value.height = videoRef.value.clientHeight + 10
   initFabric()
 }
 /**
@@ -127,33 +99,35 @@ const setVideoPlay = () => {
 const setVideoPause = () => {
   videoRef.value.pause()
 }
-watch(() => props.videoUrl, () => {
-  init()
-})
 defineExpose({setVideoPause, setVideoPlay, setCurrentTime})
 </script>
 <style lang="less" scoped>
 .video-ref {
   width: 100%;
   height: 100%;
-  display: flex;
-  justify-content: center;
+  position: relative;
   .video-proxy {
-    height: 100%;
+    height: calc(100% - 10px);
+    position: absolute;
     width: auto;
-    position: relative;
-    .canvas-proxy {
+    left: 50%;
+    top: 5px;
+    transform: translateX(-50%);
+    .videoRef {
+      height: 100%;
       width: auto;
-      height: 100%;
-      position: absolute;
-      z-index: 2000;
-      left: 50%;
-      top: 0;
-      transform: translate(-50%);
     }
-    .video-player {
-      height: 100%;
-    }
+  }
+  .canvas-proxy {
+    height: 100%;
+    width: 800px;
+    position: absolute;
+    left: 50%;
+    top: 0px;
+    transform: translateX(-50%);
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 }
 </style>
