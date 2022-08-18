d8e0632d0a22362bd3926cf24a2289762468d778
刘永志 <alittlesuperman@163.com>
2022-08-18 13:27:58 +0800

    限制剪裁区域

--- src/pages/video-editor/model/video.vue
+++ src/pages/video-editor/model/video.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="video-ref">
     <div class="video-proxy">
-      <video class="videoRef" ref="videoRef" @canplaythrough="canplaythrough" @timeupdate="timeUpdate">
-        <source :src="videoUrl">
+      <video
+        class="videoRef"
+        ref="videoRef"
+        @canplaythrough="canplaythrough"
+        @timeupdate="timeUpdate"
+      >
+        <source :src="videoUrl" />
       </video>
     </div>
     <div class="canvas-proxy">
@@ -11,95 +16,130 @@
   </div>
 </template>
 <script lang="ts" setup>
-import {defineProps, ref, reactive, watch, defineExpose, nextTick} from "vue"
-import { fabric } from "fabric"
-const videoRef = ref()
-const canvasRef = ref()
+import { defineProps, ref, reactive, watch, defineExpose, nextTick } from "vue";
+import { fabric } from "fabric";
+const videoRef = ref();
+const canvasRef = ref();
 interface Props {
   videoUrl: string;
   setCurrentTime: (currentTime: number) => void;
   setDuration: (duration: number) => void;
 }
 interface Reactive {
- n: number;
- canvas: fabric.Canvas | null;
- rect: fabric.Rect | null;
- cWidth: number;
- cHeight: number;
+  n: number;
+  canvas: fabric.Canvas | null;
+  rect: fabric.Rect | null;
+  cWidth: number;
+  cHeight: number;
 }
 const data = reactive<Reactive>({
   n: 1,
   canvas: null,
   cWidth: 0,
   cHeight: 0,
-  rect: null
-})
-const props = defineProps<Props>()
+  rect: null,
+});
+const props = defineProps<Props>();
 const init = () => {
-  data.n = Math.random()
-}
+  data.n = Math.random();
+};
 /**
  * 初始化
  */
 const initFabric = () => {
-  data.canvas = new fabric.Canvas('canvasRef')
+  data.canvas = new fabric.Canvas("canvasRef");
   data.rect = new fabric.Rect({
-    type: 'rect',
+    type: "rect",
     cornerSize: 10,
-    borderColor: '#0984e3',
-    cornerStyle: 'circle',
-    cornerColor: '#55efc4',
+    borderColor: "#0984e3",
+    cornerStyle: "circle",
+    cornerColor: "#55efc4",
     hasControls: true,
-    backgroundColor: 'rgba(255, 255, 255, 0)',
+    backgroundColor: "rgba(255, 255, 255, 0)",
     left: 5,
     top: 5,
     width: videoRef.value.clientWidth - 1,
     height: videoRef.value.clientHeight - 1,
-    fill: 'rgba(255, 255, 255, 0)',
-    minScaleLimit: 0.1
-  }) as fabric.Rect
+    fill: "rgba(255, 255, 255, 0)",
+    minScaleLimit: 0.1,
+  }) as fabric.Rect;
   data.rect.setControlsVisibility({
-    mtr: false
-  })
-  data.canvas.add(data.rect)
-  data.canvas.setActiveObject(data.rect)
+    mtr: false,
+  });
+  data.canvas.add(data.rect);
+  data.canvas.setActiveObject(data.rect);
+  initCanvasEvent()
+}
+
+/**
+ * 初始化画布事件
+ * @param e
+ */
+const initCanvasEvent = () => {
+  const target = data.canvas as fabric.Canvas
+  target.on("object:moving", e => displayMoveArea(e));
+}
+
+/**
+ * 限制剪裁拖动区域
+ * @param e
+ */
+const displayMoveArea = (e: any) => {
+  const padding = 5
+  const target = e.target
+  target.setCoords()
+  const { left, top, height, width } = target.getBoundingRect()
+  const {width: canvasWidth, height: canvasHeight} = target.canvas
+  const cWidth = canvasWidth - padding
+  const cHeight = canvasHeight - padding
+  if (left < padding || top < padding ) {
+    target.left = Math.max(target.left, padding)
+    target.top = Math.max(target.top, padding)
+  }
+  if (
+    (top + height > cHeight - padding) ||
+      (left + width > cWidth - padding)
+  ) {
+    target.top = Math.min(target.top, cHeight - height + target.top - top)
+    target.left = Math.min(target.left, cWidth - width + target.left - left)
+  }
 }
 
 /**
  * 视频加载完毕
  */
 const canplaythrough = () => {
-  props.setDuration(videoRef.value.duration)
-  canvasRef.value.width = videoRef.value.clientWidth + 10
-  canvasRef.value.height = videoRef.value.clientHeight + 10
-  initFabric()
-}
+  props.setDuration(videoRef.value.duration);
+  canvasRef.value.width = videoRef.value.clientWidth + 10;
+  canvasRef.value.height = videoRef.value.clientHeight + 10;
+  initFabric();
+};
 /**
  * 播放器时间更新
  */
 const timeUpdate = () => {
-  props.setCurrentTime(videoRef.value.currentTime)
-}
+  props.setCurrentTime(videoRef.value.currentTime);
+};
 /**
  * 设置视频当前播放进度
  * @param currentTime
  */
 const setCurrentTime = (currentTime: number) => {
-  videoRef.value.currentTime = currentTime
-}
+  videoRef.value.currentTime = currentTime;
+};
 /**
  * 播放器视频
  */
 const setVideoPlay = () => {
-  videoRef.value.play()
-}
+  videoRef.value.play();
+};
 /**
  * 暂停视频
  */
 const setVideoPause = () => {
-  videoRef.value.pause()
-}
-defineExpose({setVideoPause, setVideoPlay, setCurrentTime})
+  videoRef.value.pause();
+};
+defineExpose({ setVideoPause, setVideoPlay, setCurrentTime });
 </script>
 <style lang="less" scoped>
 .video-ref {
