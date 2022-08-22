55399533cef2ab4988f69a97c3cd5048bee33aa1
刘永志 <alittlesuperman@163.com>
2022-08-22 18:44:28 +0800

    build

--- src/utils/font.ts
+++ src/utils/font.ts
@@ -1,5 +1,15 @@
-export const loadFont = (family: string, font: string) => {
-  const f = new FontFace(family, font)
-  document.fonts.add(f)
+/**
+ * 加载字体
+ * @param key
+ * @param arrayBuffer
+ * @returns {Promise<void>}
+ */
+export const loadFont = async(fontFamily: string, arrayBuffer: ArrayBuffer) => {
+  try {
+    const font = new FontFace(fontFamily, arrayBuffer, {})
+    const face = await font.load()
+    document.fonts.add(face)
+  } catch (e) {
+    console.error('字体加载失败:', e)
+  }
 }
-
